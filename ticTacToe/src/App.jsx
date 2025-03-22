dc127d55f9f4e89f92524e6bb44fdc7a24b760ed
GopinathSM17 <58787612+GopinathSM17@users.noreply.github.com>
2025-03-22 16:51:29 +0530

    tic tae toe project created

---
+++ ticTacToe/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Board from "./components/Board.jsx";
+
+const App = () => {
+  return (
+    <div className='flex justify-center'>Gopinath SM</div>
+  )
+}
+
+export default App
