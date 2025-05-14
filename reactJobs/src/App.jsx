b7d3f120c38e2756fe4c12ee65aeda166eb48f15
Gopinath SM <gopinathsm@Gopinaths-MacBook-Air.local>
2025-05-14 18:02:21 +0530

    commit check nothing added

--- reactJobs/src/App.jsx
+++ reactJobs/src/App.jsx
@@ -5,6 +5,7 @@ import Board from './components/Board.jsx';
 const App = () => {
   return (
     <div className='flex flex-col items-center pt-10'>
+      {/* this is the gopinath code we are updating */}
       <Board />
     </div>
   )
